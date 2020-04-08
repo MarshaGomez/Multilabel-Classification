a78904d3336b849bd368c1a2bd534de946b12476
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-04-08 14:27:33 +0200

    Initial commit

---
+++ spider.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_data(url):
+    print("TODO")
+
+def main():
+    print("TODO")
+
+
+if __name__ == "__main__":
+    main()
+
+
