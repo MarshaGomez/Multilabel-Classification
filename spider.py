04d3d87e751a99aef0833d11c5f6a02fe11f9499
amaryllis55 <56350788+amaryllis55@users.noreply.github.com>
2020-04-08 21:41:16 +0200

    get category

--- spider.py
+++ spider.py
@@ -1,19 +1,15 @@
 
 import pandas as pd
 import csv
+import re
 import requests
 from bs4 import BeautifulSoup
 
 
-def get_data(url):
-    print(url)
-    #request page
-    #get_category_from_column
 
-def main():
-    get_data("www.wikipedia.it")
-
-if __name__ == "__main__":
-    main()
-
-
+if __name__ == '__main__':
+    vgm_url = 'https://www.wikihow.com/Be-Talented-in-Multiple-Areas'
+    html_text = requests.get(vgm_url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    tec=soup.find_all("a", href=re.compile("Category:"))
+    print((tec[0]["title"])[9:])
