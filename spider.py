38c947179e7be2f91f5d3102f3dc8b37364d3a5c
mgomezgomez <marsha.gomez0906@gmail.com>
2020-04-11 12:21:35 +0200

    Push SnowBall Co-authored-by: Daria Maggi <amarylliscarta@gmail.com>

--- spider.py
+++ spider.py
@@ -8,16 +8,46 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import SnowballStemmer
-
+from langdetect import detect
 from bs4 import BeautifulSoup
-
 from link_harvester import category
+
+
+# Clean Description attribute. Apply Stopwords and Stem
+def cleanDescription(description):
+    description = description.getText().lower() 
+    description = re.sub("\\b\\w{0,2}\\b|[^a-zA-Z ]", " ", description)
+    description = re.sub("([^a-zA-Z\\s+\\w]|\\s+)", " ", str(description))
+
+    if description != ' ' and description != '':
+        lang = detect(description)
+
+        if lang == 'en':
+            stop_words = set(stopwords.words('english'))
+            word_tokens = word_tokenize(description)
+
+            filtered_sentence = []
+            [filtered_sentence.append(w) for w in word_tokens if w not in stop_words ]
+            
+
+            stemmer = SnowballStemmer('english')
+            stemSentence = ''
+            for word in filtered_sentence:
+                stem = stemmer.stem(word)
+                stemSentence += stem
+                stemSentence += ' '
+            description = re.sub("\\b\\w{0,2}\\b|[^a-zA-Z ]", " ", str(stemSentence))
+            description = re.sub("([^a-zA-Z\\s+\\w]|\\s+)", " ", str(description))
+            return description
+        else:
+            return ''
+    else:
+        return ''
 
 def getText(soup):
     tex=soup.find_all("div", {"class": "step"})
-    tex=[it.getText().lower() for it in tex]
-    tex=[re.sub("\\b\\w{0,2}\\b|[^a-zA-Z ]", "", it) for it in tex]
-    tex=[re.sub("([^a-zA-Z\\s+\\w]|\\s+)", " ", str(it)) for it in tex]
+    [cleanDescription(it) for it in tex]
+    
     return " ".join(tex)
     # tex= [re.sub("\\b\\w{0,2}\\b|[^a-zA-Z ]", " ", it) for it in tex]
     #tex=[re.sub("([^a-zA-Z\\s+\\w]|\\s+)", " ", str(it)) for it in tex]
